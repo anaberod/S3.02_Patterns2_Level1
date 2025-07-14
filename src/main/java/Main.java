436451148e8c35f3cee8a42155f23dbb3fb79570
Ana Beuzón <anaberod@gmail.com>
2025-07-14 19:52:02 +0200

    Clean up project structure and update with corrected Builder version

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,32 @@
-import PizzaBuilders.FourCheeseBuilder;
-import PizzaBuilders.HawaiianPizzaBuilder;
-import PizzaBuilders.Pizza;
-import PizzaBuilders.PizzaBuilder;
+import Builders.*;
+import Pizza.Pizza;
 
 public class Main {
     public static void main(String[] args) {
         PizzaBuilder hawaiianBuilder = new HawaiianPizzaBuilder();
         PizzaChef chef = new PizzaChef(hawaiianBuilder);
-        Pizza HawaiianPizza = chef.makePizza();
-        System.out.println(HawaiianPizza);
+        Pizza hawaiianPizza = chef.makePizza();
+        System.out.println("Hawaiian pizza: ");
+        System.out.println(hawaiianPizza);
 
 
         PizzaBuilder fourCheeseBuilder = new FourCheeseBuilder();
         PizzaChef chef1 = new PizzaChef(fourCheeseBuilder);
-        Pizza FourcheesePizza = chef1.makePizza();
-        System.out.println(FourcheesePizza);
+        Pizza fourcheesePizza = chef1.makePizza();
+        System.out.println("Four cheese pizza: ");
+        System.out.println(fourcheesePizza);
+
+
+
+    Pizza customPizza = new FlexiblePizzaBuilder()
+            .setDough("Super dough")
+            .setSize("Familiar size")
+            .addTopping("Peperonni")
+            .addTopping("Tuna")
+
+            .build();
+    System.out.println("Custom pizza ");
+        System.out.println(customPizza);
 
     }
 }
--- src/main/java/PizzaBuilders/PizzaBuilder.java
+++
@@ -1,8 +0,0 @@
-package PizzaBuilders;
-
-public interface PizzaBuilder {
-    void setSize();
-    void setDough();
-    void addToppings();
-    Pizza getPizza();
-}
